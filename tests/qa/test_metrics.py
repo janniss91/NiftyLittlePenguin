1e025259538b56357bccc3c3e4b7b0dd5643641c
Jannis Spiekermann <janniss@hotmail.de>
2023-03-30 18:34:59 +0200

    Fix metrics tests

--- tests/qa/test_metrics.py
+++ tests/qa/test_metrics.py
@@ -6,37 +6,39 @@ from niftylittlepenguin.qa.metrics import QAMetrics
 
 class TestQAMetrics:
     @pytest.fixture
-    def metrics_obj(self):
+    def metrics(self):
         # (batch=3, seq_len=3)
         start_logits = torch.tensor([[0.3, 0.2, 0.5], [0.1, 0.8, 0.1], [0.4, 0.5, 0.1]])
         end_logits = torch.tensor([[0.3, 0.2, 0.5], [0.1, 0.8, 0.1], [0.1, 0.8, 0.1]])
         start_gold = torch.tensor([2, 1, 1])
         end_gold = torch.tensor([2, 0, 1])
 
-        return QAMetrics(
-            start_logits=start_logits,
-            end_logits=end_logits,
-            start_gold=start_gold,
-            end_gold=end_gold,
-        )
-
+        return start_logits, end_logits, start_gold, end_gold
+    
     @pytest.fixture
-    def metrics_obj_all_wrong(self):
-        # (batch=3, seq_len=3)
+    def metrics_all_wrong(self):
         start_logits = torch.tensor([[0.3, 0.2, 0.5], [0.1, 0.8, 0.1], [0.4, 0.5, 0.1]])
         end_logits = torch.tensor([[0.3, 0.2, 0.5], [0.1, 0.8, 0.1], [0.1, 0.8, 0.1]])
         start_gold = torch.tensor([2, 1, 0])
         end_gold = torch.tensor([1, 0, 1])
 
-        return QAMetrics(
-            start_logits=start_logits,
-            end_logits=end_logits,
-            start_gold=start_gold,
-            end_gold=end_gold,
-        )
+        return start_logits, end_logits, start_gold, end_gold
 
-    def test_acc_whole_question(self, metrics_obj):
-        assert metrics_obj.acc_whole_question() == 2 / 3
+    def test_acc_whole_question(self, metrics):
+        start_logits, end_logits, start_gold, end_gold = metrics
 
-    def test_acc_whole_question_all_wrong(self, metrics_obj_all_wrong):
-        assert metrics_obj_all_wrong.acc_whole_question() == 0
+        metrics_obj = QAMetrics()
+        start_pred = start_logits.argmax(dim=1)
+        end_pred = end_logits.argmax(dim=1)
+        acc = metrics_obj.acc_whole_question(start_pred, end_pred, start_gold, end_gold)
+        assert acc == 2 / 3
+
+    def test_acc_whole_question_all_wrong(self, metrics_all_wrong):
+        start_logits, end_logits, start_gold, end_gold = metrics_all_wrong
+
+        metrics_obj = QAMetrics()
+        start_pred = start_logits.argmax(dim=1)
+        end_pred = end_logits.argmax(dim=1)
+        acc = metrics_obj.acc_whole_question(start_pred, end_pred, start_gold, end_gold)
+
+        assert acc == 0
